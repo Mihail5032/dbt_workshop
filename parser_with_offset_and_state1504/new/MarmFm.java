c7f89304fbc433b7bebaf2a884dc01b6da0c2084
Mihail5032 <mihailsiskanov5101@gmail.com>
2026-04-20 22:30:41 +0300

    Add files via upload

---
+++ parser_with_offset_and_state1504/new/MarmFm.java
@@ -0,0 +1,18 @@
+package ru.x5.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MarmFm {
+    private String umrez;
+    private String matnr;
+    private String meinh;
+}
